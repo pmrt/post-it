8ebb0b66dbfd9c071b73d2eadb38a79184ff6016
Pedro J. Martínez <pedrojmrt@gmail.com>
2016-12-01 23:36:30 +0100

    added draw() for Post-It View

--- view/script/graph.js
+++ view/script/graph.js
@@ -1,3 +1,17 @@
 class PostItView {
 
+	constructor() {
+	}
+
+	draw() {
+		let me = document.createElement('div');
+		let content = document.createElement('p');
+		let delbtn = document.createElement('img');
+
+		me.className = 'post-it';
+
+		me.appendChild(content);
+		me.appendChild(delbtn);
+		document.getElementsByClassName('board')[0].appendChild(me);
+	}
 }
