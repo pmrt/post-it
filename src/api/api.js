818054358886f8f037db0ca4fa529a3219d7526d
Pedro J. Martínez <pedrojmrt@gmail.com>
2016-12-05 23:42:39 +0100

    Irrv: some irrelevant changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,65 @@
-class PostIt {
+class API {
 
 	static addBlank() {
-		let postit = new PostItView()
-		          .draw();
+		/*
+			Add a blank Post-It.
+			Instances a Post-It View Object,
+			draws it and watch it.
+		*/
+		let PostItObjView = new PostItView();
+		let PostItObj = new $PostIt();
+		PostItObjView.draw();
+		API.watchPostIt(PostItObjView);
 	}
 
+	static watchPostIt(PostItObjView) {
+		/*
+			Add the events to the passed PostItObjView.
+		*/
+		PostItObjView.element.addEventListener('click', function(e) {
+				API.editActive(e);
+			});
+		PostItObjView.element.addEventListener('focusout', function(e){
+				API.editInactive(e);
+			});
+	}
+
+	static watchBoard(BoardView) {
+		/*
+			Add the events to the passed BoardView.
+		*/
+		BoardView.title.addEventListener('click', function(e){
+
+		});
+	}
+
+	static editActive(e) {
+		/*
+			Enable the edit mode for the passed event.
+		*/
+		if (!e.currentTarget.obj.editEnabled) {
+			e.currentTarget.obj.editActive();
+		}
+	}
+
+	static editInactive(e) {
+		/*
+			Disable the edit mode for the passed event.
+		*/
+		if (e.currentTarget.obj.editEnabled) {
+			e.currentTarget.obj.editInactive();
+		}
+	}
+
+	static createBoard() {
+		let board = new Board();
+		let boardView = new BoardView();
+		boardView.draw();
+
+		$watchClass(PostIt, function(){
+			board.addPostIt(this);
+		});
+
+		return board;
+	}
 }
--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,33 @@
 class Board {
 
-	constructor(title) {
-		this.title = title || 'My Post-It Board';
+	constructor() {
+		this.title = 'My Post-It Board';
+		this.postIts = [];
+	}
+
+	set postIt(postItObj) {
+		this.postIts[this.getLastId()+1+""] = postItObj;
+	}
+
+	// getLastId() {
+	// 	let ids = Object.keys(this.postIts);
+
+	// 	if (ids == "") {
+	// 		return -1;
+	// 	} else {
+	// 		return parseInt(ids[ids.length-1]);
+	// 	}
+	// }
+
+	addPostIt(postIt) {
+		this.postIts.push(postIt);
 	}
 
 }
 
 class PostIt {
 
-	constructor(title, content) {
-		this.title = title;
+	constructor(content) {
 		this.content = content;
 	}
 }
